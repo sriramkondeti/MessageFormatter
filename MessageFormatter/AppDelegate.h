d78b2b043bd29b94d1ae5aa9dec2ccb3a4f49d61
Sri Ram <sriramkondeti@gmail.com>
2017-01-18 11:02:16 +0800

    Initial Commit

---
+++ MessageFormatter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MessageFormatter
+//
+//  Created by Sri Ram on 18/01/2017.
+//  Copyright © 2017 Sri Ram. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MessageFormatterTests/MessageFormatterTests.m
@@ -0,0 +1,39 @@
+//
+//  MessageFormatterTests.m
+//  MessageFormatterTests
+//
+//  Created by Sri Ram on 18/01/2017.
+//  Copyright © 2017 Sri Ram. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface MessageFormatterTests : XCTestCase
+
+@end
+
+@implementation MessageFormatterTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
